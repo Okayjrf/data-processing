e130926dc27c56004453e8b3da8dd81f86733218
Okayjrf <okayjrf@gmail.com>
2023-01-24 22:48:49 -0800

    Final Changes

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,57 +1,62 @@
-// alert("hello from data processing!");
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
+  titleCase("Where are the bears?");
 
 const queryString = window.location.search;
 
-if(queryString.length > 0){
 
+if(queryString.length > 0){
 
     const urlParams = new URLSearchParams(queryString);
 
     let myData = "";
-    let myTotal = 0; //will store the total for cart
     let myCart = "";
+    let myTotal = 0;//will store total cost
+    myCart += "<h3>Cart Content</h3>";
 
+// Log the values
+urlParams.forEach(function(value, key) {
 
-        /* Cart Contents
+    if(key == "Cart"){//process cart
+        //alert("Cart Item: " + value);
 
-        Widget: $3.99
-        Sprocket: $5.99
-        Thingy: $1.99
+        switch(value){
+            case "Widget":
+                myCart += "Widget: $3.99<br>";
+                myTotal += 3.99;
+            break;
 
-        Total: $11.97
-        .*/
-    // Log the values
-    myCart += "<h3>Cart Contents</h3>"
-    urlParams.forEach(function(value, key){
-        if(key == "Cart"){//process cart
-            //alert("Cart Item: " + value)
-                
-            switch(value){
-                case "Widget":
-                    myCart += "Widget:$3.99<br>";
-                    myTotal += 3.99;
-                break;
-                case "Sprocket":
-                    myCart += "Sprocket:$5.99<br>";
-                    myTotal += 5.99;
-                break;
-                case "Thingy":
-                    myCart += "Thingy:$1.99<br>";
-                    myTotal += 1.99;
-                break;
-            }
+            case "Sprocket":
+                myCart += "Sprocket: $5.99<br>";
+                myTotal += 5.99;
+            break;
 
-        }else{//process shipping
-            key = key.split("_").join(" ");
-            //console.log(key, value);
-            myData += `<p>${key}: ${value}</p>`;
+            case "Thingy":
+                myCart += "Thingy: $1.99<br>";
+                myTotal += 1.99;
+            break;
         }
-    });
 
-    myCart += "Total:" + myTotal+ '<br />';
-    //we want the cart + shipping
+    }else{//process shipping
+        key = key.split("_").join(" ");
+        if(key == "First Name" || key == "Last Name"){
+            value = titleCase(value);
+        }
+       
+    myData += `<p>${key}: ${value}</p>`;
+    }
+});
+
+    myCart += "Total: " + myTotal + '<br>';
+    //we want the cart data first 
     myData = myCart + myData;
 
-    myData += '<p><a href="index.html">CLEAR</a></p>';
-    document.getElementById('output').innerHTML = myData;
+    myData += '<p><a href="index.html">CLEAR</a></p>'
+
+    document.getElementById("output").innerHTML = myData;
 }
